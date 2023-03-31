d5e908db9b92f563b116fef712a0e195f0a5cce5
Ana <ana.jananashvili.1@iliauni.edu.ge>
2023-03-31 22:36:24 +0400

    added burger menu

--- index.js
+++ index.js
@@ -1,24 +1,8 @@
+const navBurger = document.querySelector(".burger");
+const navLinks =document.querySelector(".nav-links");
 
-var modal = document.getElementById("myModal");
-
-
-var btn = document.getElementById("myBtn");
-
-
-var span = document.getElementsByClassName("close")[0];
-
-
-btn.onclick = function() {
-  modal.style.display = "block";
+function navActiveToggler () {
+  navLinks.classList.toggle("nav-links__active")
 }
 
-
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+navBurger.addEventListener("click", navActiveToggler);
